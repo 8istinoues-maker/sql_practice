bdbb1d9b01df7d0d311e9256c9f6a6efc9ffe81f
syonosuke743 <syonosuke1996@outlook.jp>
2026-07-01 22:40:36 +0900

    dockerとpostgresSQL環境の構築完了y

---
+++ init/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE world (
+name VARCHAR(100),
+continent VARCHAR(100),
+area BIGINT,
+population BIGINT,
+gdp BIGINT
+);
---
+++ init/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO world (name, continent, area, population, gdp)
+VALUES
+('Germany', 'Europe', 357022, 83000000, 4000000),
+('France', 'Europe', 551695, 67000000, 3000000),
+('Australia', 'Oceania', 7692024, 26000000, 1700000);
